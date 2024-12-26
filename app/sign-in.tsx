68360b79e4a278b4eb0df0691573be51aef73fc1
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-12-26 08:56:54 +0400

    Refactor SignIn component: enhance UI and add Google sign-in functionality
    - Updated the `SignIn` component in `sign-in.tsx` to include a more comprehensive user interface with a welcome message and a Google sign-in button.
    - Integrated `SafeAreaView` and `ScrollView` for better layout management.
    - Added logging functionality to the sign-in process using the newly introduced logger utility.
    - Improved visual elements with images and Tailwind CSS classes for a polished design.

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,10 +1,45 @@
-import { View, Text } from "react-native";
+import { Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import images from "@/constants/images";
+import icons from "@/constants/icons";
+
+import { logger } from "@/lib/logger";
 
 const SignIn = () => {
+  const handleSignIn = () => {
+    logger.info("sign in");
+  };
+
   return (
-    <View>
-      <Text>sign-in</Text>
-    </View>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView contentContainerClassName="h-full">
+        <Image source={images.onboarding} className="w-full h-2/3" />
+
+        <View className="px-10">
+          <Text className="text-base font-rubik uppercase text-center text-black-200">
+            Welcome To React Native Example
+          </Text>
+
+          <Text className="text-3xl font-rubik-bold text-black-300 text-center mt-2">
+            Let's get you closer to {"\n"}
+            <Text className="text-primary-300">Better examples</Text>
+          </Text>
+          <Text className="text-lg font-rubik text-black-200 text-center mt-12">
+            Login to React Native Example with Google
+          </Text>
+
+          <TouchableOpacity
+            className="bg-white shadow-md shadow-zinc-300 rounded-full w-full py-4 mt-5"
+            onPress={handleSignIn}
+          >
+            <View className="flex flex-row items-center gap-2 justify-center">
+              <Image source={icons.google} className="w-5 h-5" resizeMode="contain" />
+              <Text className="text-lg font-rubik-medium text-black-300">Continue with Google</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
