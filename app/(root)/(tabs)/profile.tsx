51872d1be245728f8e8d4c61a7770fdf97e8114f
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-12-28 23:39:51 +0400

    Implement user profile display and logout functionality
    - Integrated global context to fetch and display user information (name and email) in the Profile component.
    - Added a logout button that triggers a logout process and refetches user data using the logger utility for logging actions.

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,9 +1,25 @@
-import { View, Text } from "react-native";
+import { useGlobalContext } from "@/lib/global-provider";
+import { logger } from "@/lib/logger";
+import { View, Text, TouchableOpacity } from "react-native";
 
 const Profile = () => {
+  const { user, refetch } = useGlobalContext();
+
+  const handleLogout = async () => {
+    logger.info("logout");
+    refetch();
+  };
+
   return (
     <View>
       <Text>Profile</Text>
+
+      <Text>{user?.name}</Text>
+      <Text>{user?.email}</Text>
+
+      <TouchableOpacity onPress={handleLogout}>
+        <Text>Logout</Text>
+      </TouchableOpacity>
     </View>
   );
 };
