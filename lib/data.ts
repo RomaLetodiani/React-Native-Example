eded657fd260d7e0697d8b67da79a62d55cb8441
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-12-29 18:32:41 +0400

    Refactor user data fetching in data.ts to improve API simulation and error handling
    - Introduced a new `simulateApiCall` function to standardize API call simulation with a configurable delay.
    - Refactored `getCurrentUser` to utilize a generic `apiCall` function for improved error handling and consistency.
    - Added new functions `getLatestProperties`, `getProperties`, and `getPropertyById` to standardize property data fetching with error handling.

--- lib/data.ts
+++ lib/data.ts
@@ -1,18 +1,30 @@
 import { User } from "@/types/user.types";
+import { logger } from "./logger";
 
-export const getCurrentUser = async (): Promise<User | null> => {
+const API_DELAY = 2000;
+// Simulate API call with 2 second delay
+const simulateApiCall = async (ms: number = API_DELAY) =>
+  await new Promise((resolve) => setTimeout(resolve, ms));
+
+const apiCall = async (fn: () => Promise<any>) => {
   try {
-    // Simulate API call with 2 second delay
-    await new Promise((resolve) => setTimeout(resolve, 2000));
-
-    // Return fake user data
-    return {
-      id: "123",
-      name: "John Doe",
-      email: "john@example.com",
-    };
+    await simulateApiCall();
+    return await fn();
   } catch (error) {
-    console.log(error);
+    logger.error(error);
     return null;
   }
 };
+
+export const getCurrentUser = async (): Promise<User | null> =>
+  apiCall(async () => ({
+    id: "123",
+    name: "John Doe",
+    email: "john@example.com",
+  }));
+
+export const getLatestProperties = async () => apiCall(async () => []);
+
+export const getProperties = async () => apiCall(async () => []);
+
+export const getPropertyById = async () => apiCall(async () => null);
