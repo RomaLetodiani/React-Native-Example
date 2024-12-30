92d9bc9ee27e874dd89ac26344fa921609a4cbbc
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2024-12-30 23:49:52 +0400

    Refactor Profile Component to Enhance User Experience and Settings Integration
    - Introduced a new SettingsItem component for better organization of settings options.
    - Enhanced the Profile component layout with a SafeAreaView and ScrollView for improved navigation.
    - Added user profile display with an editable avatar and user details.
    - Integrated logout functionality with updated context management for user login state.
    - Included additional settings options such as "My Bookings" and "Payments" for user convenience.
    
    These changes significantly improve the user interface and experience within the profile section.

--- app/(root)/(tabs)/profile.tsx
+++ app/(root)/(tabs)/profile.tsx
@@ -1,26 +1,88 @@
+import {
+  Alert,
+  Image,
+  ImageSourcePropType,
+  SafeAreaView,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
+
 import { useGlobalContext } from "@/lib/global-provider";
-import { logger } from "@/lib/logger";
-import { View, Text, TouchableOpacity } from "react-native";
+
+import icons from "@/constants/icons";
+import { settings } from "@/constants/data";
+
+interface SettingsItemProp {
+  icon: ImageSourcePropType;
+  title: string;
+  onPress?: () => void;
+  textStyle?: string;
+  showArrow?: boolean;
+}
+
+const SettingsItem = ({ icon, title, onPress, textStyle, showArrow = true }: SettingsItemProp) => (
+  <TouchableOpacity onPress={onPress} className="flex flex-row items-center justify-between py-3">
+    <View className="flex flex-row items-center gap-3">
+      <Image source={icon} className="size-6" />
+      <Text className={`text-lg font-rubik-medium text-black-300 ${textStyle}`}>{title}</Text>
+    </View>
+
+    {showArrow && <Image source={icons.rightArrow} className="size-5" />}
+  </TouchableOpacity>
+);
 
 const Profile = () => {
-  const { user, refetch } = useGlobalContext();
+  const { user, refetch, setIsLogged } = useGlobalContext();
 
   const handleLogout = async () => {
-    logger.info("logout");
-    refetch();
+    setIsLogged(false);
   };
 
   return (
-    <View>
-      <Text>Profile</Text>
+    <SafeAreaView className="h-full bg-white">
+      <ScrollView showsVerticalScrollIndicator={false} contentContainerClassName="pb-32 px-7">
+        <View className="flex flex-row items-center justify-between mt-5">
+          <Text className="text-xl font-rubik-bold">Profile</Text>
+          <Image source={icons.bell} className="size-5" />
+        </View>
 
-      <Text>{user?.name}</Text>
-      <Text>{user?.email}</Text>
+        <View className="flex flex-row justify-center mt-5">
+          <View className="flex flex-col items-center relative mt-5">
+            <div className="size-44 relative rounded-full bg-primary-300">
+              <Text className="text-white text-2xl font-rubik-bold">{user?.name?.charAt(0)}</Text>
+            </div>
+            <TouchableOpacity className="absolute bottom-11 right-2">
+              <Image source={icons.edit} className="size-9" />
+            </TouchableOpacity>
 
-      <TouchableOpacity onPress={handleLogout}>
-        <Text>Logout</Text>
-      </TouchableOpacity>
-    </View>
+            <Text className="text-2xl font-rubik-bold mt-2">{user?.name}</Text>
+          </View>
+        </View>
+
+        <View className="flex flex-col mt-10">
+          <SettingsItem icon={icons.calendar} title="My Bookings" />
+          <SettingsItem icon={icons.wallet} title="Payments" />
+        </View>
+
+        <View className="flex flex-col mt-5 border-t pt-5 border-primary-200">
+          {settings.slice(2).map((item, index) => (
+            <SettingsItem key={index} {...item} />
+          ))}
+        </View>
+
+        <View className="flex flex-col border-t mt-5 pt-5 border-primary-200">
+          <SettingsItem
+            icon={icons.logout}
+            title="Logout"
+            textStyle="text-danger"
+            showArrow={false}
+            onPress={handleLogout}
+          />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
